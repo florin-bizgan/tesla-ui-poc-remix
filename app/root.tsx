a411e28ef4f2c8ebba89215b967d98769cef9419
Florin <florinbizgan2000@gmail.com>
2023-01-09 22:23:45 +0000

    changed name of project

--- app/root.tsx
+++ app/root.tsx
@@ -10,13 +10,13 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Tesla UI PoC Remix",
   viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
